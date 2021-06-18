44a4842092938c765039242be0b417e5cb8dc0a0
lucianohuenuvil <lucianobvx@gmail.com>
2021-06-18 18:59:36 -0400

    paginacion y nuevos productos

--- js/agregarproducto.js
+++ js/agregarproducto.js
@@ -1,19 +1,35 @@
 let productos = [
     {
-      "nombre": "Notebook Asus",
-      "id": "notebookasus",
-      "precio": 559900,
+        "nombre": "Notebook Asus",
+        "id": "notebookasus",
+        "precio": 559900,
     },
     {
-    "nombre": "Monitor Gear",
-    "id": "monitorgear",
-    "precio": 125000,
+        "nombre": "Monitor Gear",
+        "id": "monitorgear",
+        "precio": 125000,
     },
     {
-    "nombre": "Xbox Series S",
-    "id": "xboxseriess",
-    "precio": 359990,
-    }
+        "nombre": "Xbox Series S",
+        "id": "xboxseriess",
+        "precio": 359990,
+    },
+    {
+        "nombre": "Notebook HP",
+        "id": "notebookhp",
+        "precio": 299990,
+    },
+    {
+        "nombre": "Playstation 4",
+        "id": "consolaps4",
+        "precio": 209990,
+    },
+    {
+        "nombre": "Tv lg",
+        "id": "tvledlg",
+        "precio": 209990,
+    },
+ 
 ]
 
 var productosselect = new Array();
