aa457e5c3b1de65f29dce2e0fa4cccd0f5dd300f
lucianohuenuvil <lucianobvx@gmail.com>
2021-06-18 14:26:20 -0400

    Creacion proyecto

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+
+function navresponsive(){
+    if (document.querySelector(".link-nav-container-responsive").style.left == "-100%"){
+        document.querySelector(".link-nav-container-responsive").style.left = 0;
+    } 
+    else{
+        document.querySelector(".link-nav-container-responsive").style.left = "-100%";
+    }
+    
+}
